4fec118cf87dc9d66d87da777d12b9b3617c1513
Daniel <danieltadashi2004@gmail.com>
2024-10-06 02:22:12 -0500

    Base funcional de la aplicacion web

--- app.py
+++ app.py
@@ -1,35 +1,34 @@
-from flask import Flask, request, g
-import sqlite3
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-DATABASE = 'mi_base_de_datos.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mi_base_de_datos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+class Mensaje(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    mensaje = db.Column(db.Text, nullable=False)
 
 @app.route('/')
 def home():
-    return 'Servidor funcionando'
+    return render_template('principal.html')
 
 @app.route('/submit', methods=['POST'])
 def submit_form():
-    nombre = request.json.get('nombre')
-    email = request.json.get('email')
-    mensaje = request.json.get('mensaje')
+    nombre = request.form['nombre']
+    email = request.form['email']
+    mensaje = request.form['mensaje']
 
-    db = get_db()
-    db.execute('INSERT INTO mensajes (nombre, email, mensaje) VALUES (?, ?, ?)', (nombre, email, mensaje))
-    db.commit()
-    return 'Formulario enviado'
+    nuevo_mensaje = Mensaje(nombre=nombre, email=email, mensaje=mensaje)
+    db.session.add(nuevo_mensaje)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, host='0.0.0.0', port=5000)
+
